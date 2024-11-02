fa2242628d5483eaf21367c45ec86d8f699ccf92
Teguh Samudra <teguhsam9@gmail.com>
2024-11-02 10:30:54 -0700

    refine FlightAI chatbot

--- main.py
+++ main.py
@@ -2,35 +2,72 @@ import os
 from dotenv import load_dotenv
 from openai import OpenAI
 import gradio as gr
-# import google.generativeai 
-# import anthropic
+import json
 
+# Load Model
 load_dotenv()
 os.environ['OPENAI_API_KEY'] = os.getenv('OPENAI_API_KEY')
-os.environ['ANTHROPIC_API_KEY'] = os.getenv('ANTHROPIC_API_KEY')
-os.environ['GOOGLE_API_KEY'] = os.getenv('GOOGLE_API_KEY')
-
 openai = OpenAI()
-OPENAI_MODEL = "gpt-4o-mini"
+MODEL = "gpt-4o-mini"
 
-system_message = "You are a helpful assistant in a clothes store. You should try to gently encourage \
-the customer to try items that are on sale. Hats are 60% off, and most other items are 50% off. \
-For example, if the customer says 'I'm looking to buy a hat', \
-you could reply something like, 'Wonderful - we have lots of hats - including several that are part of our sales evemt.'\
-Encourage the customer to buy hats if they are unsure what to get. If the customer asks for shoes, you should respond that shoes are not on sale today, \
-but remind the customer to look at hats!"
+# Define assistant's system message
+system_message = "You are a helpful assistant for an Airline called FlightAI. "
+system_message += "Give short, courteous answers, no more than 1 sentence. "
+system_message += "Always be accurate. If you don't know the answer, say so."
+
+# Tool: A look up table for assistant to check ticket prices 
+ticket_prices = {"london": "$799", "paris": "$899", "tokyo": "$1400", "berlin": "$499"}
+
+def get_ticket_price(destination_city):
+    city = destination_city.lower()
+    return ticket_prices.get(city, "Unknown")
+
+price_function = {
+    "name": "get_ticket_price",
+    "description": "Get the price of a return ticket to the destination city. Call this whenever you need to know the ticket price, for example when a customer asks 'How much is a ticket to this city'",
+    "parameters": {
+        "type": "object",
+        "properties": {
+            "destination_city": {
+                "type": "string",
+                "description": "The city that the customer wants to travel to",
+            },
+        },
+        "required": ["destination_city"],
+        "additionalProperties": False
+    }
+}
+
+# Specify tool for chatbot
+tools = [{"type": "function", "function": price_function}]
+def handle_tool_call(message):
+    tool_call = message.tool_calls[0]
+    arguments = json.loads(tool_call.function.arguments)
+    city = arguments.get('destination_city')
+    price = get_ticket_price(city)
+    response = {
+        "role": "tool",
+        "content": json.dumps({"destination_city": city,"price": price}),
+        "tool_call_id": message.tool_calls[0].id
+    }
+    return response, city
+
+# Chatbot
 def chat(message, history):
     messages = [{"role": "system", "content": system_message}]
-    for user_message, assistant_message in history:
-        messages.append({"role": "user", "content": user_message})
-        messages.append({"role": "assistant", "content": assistant_message})
+    for human, assistant in history:
+        messages.append({"role": "user", "content": human})
+        messages.append({"role": "assistant", "content": assistant})
     messages.append({"role": "user", "content": message})
+    response = openai.chat.completions.create(model=MODEL, messages=messages, tools=tools)
 
-    stream = openai.chat.completions.create(model=OPENAI_MODEL, messages=messages, stream=True)
-
-    response = ""
-    for chunk in stream:
-        response += chunk.choices[0].delta.content or ''
-        yield response
+    if response.choices[0].finish_reason=="tool_calls":
+        message = response.choices[0].message
+        response, city = handle_tool_call(message)
+        messages.append(message)
+        messages.append(response)
+        response = openai.chat.completions.create(model=MODEL, messages=messages)
+    
+    return response.choices[0].message.content
 
 gr.ChatInterface(fn=chat).launch()
